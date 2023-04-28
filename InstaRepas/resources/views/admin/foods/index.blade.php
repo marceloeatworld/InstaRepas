451d8fe7aa10513382b16fcdc0077f293f7388a7
Marcelo <marcelo.ben@pm.me>
2023-04-28 15:40:51 +0200

    edit foods admin

--- InstaRepas/resources/views/admin/foods/index.blade.php
+++ InstaRepas/resources/views/admin/foods/index.blade.php
@@ -12,5 +12,27 @@
             <li><a href="{{ route('admin.foods.index') }}">Food</a></li>
         </ul>
     </nav>
+
+    <h1>Food List</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($foods as $food)
+                <tr>
+                    <td>{{ $food->id }}</td>
+                    <td>{{ $food->name }}</td>
+                    <td>
+                        <a href="{{ route('admin.foods.edit', $food->id) }}">Edit</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
