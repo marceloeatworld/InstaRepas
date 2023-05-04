41c6116eb82fea5f7c12acb2e4a52dc24e03098b
Marcelo <marcelo.ben@pm.me>
2023-05-04 21:29:49 +0200

    some fix drk theme removed, logo tmp ...

--- InstaRepas/resources/views/welcome.blade.php
+++ InstaRepas/resources/views/welcome.blade.php
@@ -25,6 +25,7 @@
                     <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                    
                     <a href="{{ url('/admin') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Admin</a>   
+                 
                     @else
                         <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
