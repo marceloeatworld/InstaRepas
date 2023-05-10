a1fda41a8a1e0f8a5726f86220f25ae5871d7a5d
Marcelo <marcelo.ben@pm.me>
2023-05-10 19:26:55 +0200

    st

--- InstaRepas/resources/views/meals/meals.blade.php
+++ InstaRepas/resources/views/meals/meals.blade.php
@@ -1,46 +1,48 @@
 <x-app-layout>
 
-<div class="max-w-2xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden p-6">
-  <h1 class="text-2xl font-bold mb-2">Meal Plan</h1>
-  <p class="mb-2">Date: {{ $current_date }}</p>
-  <p class="mb-4">Season: {{ $current_season }}</p>
-  @for($day = 0; $day < count($breakfasts); $day++)
-    <div class="bg-gray-100 p-4 rounded-lg my-4">
-      <h3 class="text-xl font-bold mb-2">Day {{ $day + 1 }}</h3>
+<div class="flex flex-wrap h-screen w-full overflow-auto">
+    <h1 class="w-full text-center text-3xl pb-4">Meal Plan</h1>
+    <p class="w-full text-center pb-2">Date: {{ $current_date }}</p>
+    <p class="w-full text-center pb-4">Season: {{ $current_season }}</p>
+    @for($day = 0; $day < count($breakfasts); $day++)
+        <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+            <div class="bg-white shadow-lg rounded-lg overflow-hidden p-6 flex flex-col">
+                <h3 class="text-xl mb-4">Day {{ $day + 1 }}</h3>
 
-      <h4 class="text-lg font-semibold mb-1">Breakfast</h4>
-      <div class="mb-2">
-        Protein: {{ $breakfasts[$day]['protein']->name }}<br>
-        Carbohydrate: {{ $breakfasts[$day]['carbohydrate']->name }}<br>
-        Fruit: {{ $breakfasts[$day]['fruit']->name }}
-      </div>
+                <h4 class="text-lg mb-2">Breakfast</h4>
+                <div class="mb-4">
+                    Protein: {{ $breakfasts[$day]['protein']->name }}<br>
+                    Carbohydrate: {{ $breakfasts[$day]['carbohydrate']->name }}<br>
+                    Fruit: {{ $breakfasts[$day]['fruit']->name }}
+                </div>
 
-      <h4 class="text-lg font-semibold mb-1">Lunch</h4>
-      <div class="mb-2">
-        Protein: {{ isset($lunches[$day]['protein']) ? $lunches[$day]['protein']->name : 'No protein available' }}<br>
-        Carbohydrate: {{ $lunches[$day]['carbohydrate']->name }}<br>
-        Vegetable: {{ $lunches[$day]['vegetable']->name }}<br>
-        Lipid: {{ $lunches[$day]['lipid']->name }}
-      </div>
+                <h4 class="text-lg mb-2">Lunch</h4>
+                <div class="mb-4">
+                    Protein: {{ isset($lunches[$day]['protein']) ? $lunches[$day]['protein']->name : 'No protein available' }}<br>
+                    Carbohydrate: {{ $lunches[$day]['carbohydrate']->name }}<br>
+                    Vegetable: {{ $lunches[$day]['vegetable']->name }}<br>
+                    Lipid: {{ $lunches[$day]['lipid']->name }}
+                </div>
+                @if($include_snacks)
+                    <h4 class="text-lg mb-2">Snack</h4>
+                    <div class="mb-4">
+                        Fruit: {{ $snacks[$day]['fruit'] ? $snacks[$day]['fruit']->name : 'No fruit available' }}<br>
+                        Other Snack: {{ $snacks[$day]['other_snack'] ? $snacks[$day]['other_snack']->name : 'No other snack available' }}
+                    </div>
+                @endif
+                <h4 class="text-lg mb-2">Dinner</h4>
+                <div class="mb-4">
+                    Protein: {{ isset($dinners[$day]['protein']) ? $dinners[$day]['protein']->name : 'No protein available' }}<br>
+                    Carbohydrate: {{ $dinners[$day]['carbohydrate']->name }}<br>
+                    Vegetable: {{ $dinners[$day]['vegetable']->name }}<br>
+                    Lipid: {{ $dinners[$day]['lipid']->name }}
+                </div>
 
-      @if($include_snacks)
-        <h4 class="text-lg font-semibold mb-1">Snack</h4>
-        <div class="mb-2">
-          Fruit: {{ $snacks[$day]['fruit'] ? $snacks[$day]['fruit']->name : 'No fruit available' }}<br>
-          Other Snack: {{ $snacks[$day]['other_snack'] ? $snacks[$day]['other_snack']->name : 'No other snack available' }}
+            </div>
         </div>
-      @endif
-
-      <h4 class="text-lg font-semibold mb-1">Dinner</h4>
-      <div>
-        Protein: {{ isset($dinners[$day]['protein']) ? $dinners[$day]['protein']->name : 'No protein available' }}<br>
-        Carbohydrate: {{ $dinners[$day]['carbohydrate']->name }}<br>
-        Vegetable: {{ $dinners[$day]['vegetable']->name }}<br>
-        Lipid: {{ $dinners[$day]['lipid']->name }}
-      </div>
-    </div>
-  @endfor
+    @endfor
 </div>
 
-    </x-app-layout>
+
+</x-app-layout>
 
