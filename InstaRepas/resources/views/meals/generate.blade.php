fbe5723c53ad47cf2c6655e79084d54d21a94c9c
Marcelo <marcelo.ben@pm.me>
2023-05-23 16:55:22 +0200

    gen ui,pref ui,meal ui

--- InstaRepas/resources/views/meals/generate.blade.php
+++ InstaRepas/resources/views/meals/generate.blade.php
@@ -1,42 +1,39 @@
 <x-app-layout>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="w-full p-6 border border-gray-200 rounded-lg shadow flex items-center justify-center text-center" style="background-color: #6495ED;">
-
-                <a href="{{ route('admin.users.index') }}">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white mx-4 tracking-wider">Préférences alimenaites</h5>
-                </a>
-
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center">
+            <div class="w-full md:w-2/3 lg:w-1/2 p-6 rounded-lg flex items-center justify-center text-center bg-gradient-to-r from-blue-900 to-blue-400">
+                <h5 class="mb-2 text-2xl font-semibold tracking-normal text-white mx-4">Savourez l'expérience</h5>
             </div>
+        </div>
 
-            <form action="{{ route('generate_meals') }}" method="post">
+            <form action="{{ route('generate_meals') }}" method="post" class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg px-6 py-4 mx-auto w-full md:w-2/3 lg:w-1/2">
                 @csrf
                 <fieldset>
-        <legend>Restrictions alimentaires</legend>
-        <div>
-            @foreach ($availableDietaryRestrictions as $restriction)
-                <label class="block mb-2">
-                    <input type="checkbox" name="restrictions[]" value="{{ $restriction->name }}" {{ in_array($restriction->id, $userPreferences) ? 'checked' : '' }}> {{ $displayNames[$restriction->name] }}
-                </label>
-            @endforeach
-        </div>
-
-        </fieldset>
-
-
-
-                <fieldset>
-                    <legend>Nombre de jours</legend>
-                    <input type="number" name="days" min="1" value="1">
+                    <legend class="text-lg font-semibold text-gray-700 dark:text-gray-200 mb-2 text-center">Restrictions alimentaires</legend>
+                    <div class="grid grid-cols-2 gap-4 mb-4">
+                        @foreach ($availableDietaryRestrictions as $restriction)
+                            <label class="inline-flex items-center mt-3">
+                                <input type="checkbox" class="form-checkbox h-5 w-5 text-blue-600" name="restrictions[]" value="{{ $restriction->name }}" {{ in_array($restriction->id, $userPreferences) ? 'checked' : '' }}><span class="ml-2 text-gray-700 dark:text-gray-200">{{ $displayNames[$restriction->name] }}</span>
+                            </label>
+                        @endforeach
+                    </div>
                 </fieldset>
 
-                <div style="text-align: center;">
-                    <button type="submit" class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Génerer</button>
+                <fieldset>
+                    <legend class="text-lg font-semibold text-gray-700 dark:text-gray-200 mb-2 text-center">Nombre de jours</legend>
+                    <div class="mb-4 w-1/4 mx-auto">
+                        <input type="number" name="days" min="1" value="1" class="focus:ring-blue-500 focus:border-blue-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md">
+                    </div>
+                </fieldset>
+
+                <div class="flex items-center justify-center">
+                    <button type="submit" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-500 hover:to-blue-700">Génerer</button>
                 </div>
             </form>
-            <p style="text-align: center;">Si vous souhaitez enregistrer vos préférences, veuillez vous <a href="/register">inscrire</a> ou vous <a href="/login">connecter</a>.</p>
+
+            <div class="mt-6 text-center text-sm text-gray-600">
+                Si vous souhaitez enregistrer vos préférences, veuillez vous <a href="/register" class="text-blue-600 hover:text-blue-500 underline">inscrire</a> ou vous <a href="/login" class="text-blue-600 hover:text-blue-500 underline">connecter</a>.
+            </div>
         </div>
     </div>
-
-    </x-app-layout>
+</x-app-layout>
