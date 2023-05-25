deb2809249e6dc0df057a89d2e8e5cc5935a95f2
Marcelo <marcelo.ben@pm.me>
2023-05-25 19:05:35 +0200

    fix mobile view, js ...

--- InstaRepas/resources/views/layouts/footer.blade.php
+++ InstaRepas/resources/views/layouts/footer.blade.php
@@ -1,36 +1,35 @@
-<footer class="bg-[#FFDB58] text-black-300 py-6 h-80">
-  <div class="container mx-auto">
-    <div class="flex flex-wrap justify-center">
-      <div class="w-full md:w-1/3 lg:w-1/4 px-3 mb-6">
+<footer class="bg-[#FFDB58] text-black-300 py-6 md:h-80">
+  <div class="container mx-auto px-4">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
+      <div class="mb-6 md:mb-0 px-3">
         <h4 class="text-lg font-bold mb-3">Liens rapides</h4>
-        <ul class="list-reset leading-loose">
+        <ul class="list-reset leading-loose text-black-600">
           <li class="mb-2"><a href="/" class="hover:text-gray-500">Accueil</a></li>
           <li class="mb-2"><a href="/contact" class="hover:text-gray-500">Nous-contacter</a></li>
           <li class="mb-2"><a href="/a-propos" class="hover:text-gray-500">À propos</a></li>
           <li class="mb-2"><a href="{{ route('legal') }}" class="hover:text-gray-500">Mentions légales</a></li>
         </ul>
       </div>
-      <div class="w-full md:w-1/3 lg:w-1/4 px-3 mb-6">
+      <div class="mb-6 md:mb-0 px-3">
         <h4 class="text-lg font-bold mb-3">Suivez-nous</h4>
-        <ul class="list-reset leading-loose">
+        <ul class="list-reset leading-loose text-black-600">
           <li class="mb-2"><a href="https://www.instagram.com/foodequilibre/" class="flex items-center hover:text-gray-500"><i class="fab fa-instagram mr-2"></i> Instagram foodequilibre</a></li>
           <li class="mb-2"><a href="https://www.instagram.com/painetchocolat_asso/" class="flex items-center hover:text-gray-500"><i class="fab fa-instagram mr-2"></i> Instagram pain et chocolat</a></li>
         </ul>
       </div>
-      <div class="w-full md:w-1/3 lg:w-1/4 px-3 mb-6">
+      <div class="px-3">
         <h4 class="text-lg font-bold mb-3">Cabinet de Diététique</h4>
-        <ul class="list-reset leading-loose">
+        <ul class="list-reset leading-loose text-black-600">
           <li class="mb-2">
             <i class="fas fa-map-marker-alt mr-2"></i>
             <a href="https://www.google.com/maps/search/?api=1&query=6+rue+de+plaisance+94130+Nogent+sur+Marne" target="_blank">6 rue de plaisance 94130 Nogent sur Marne</a>
           </li>
-         <li class="mb-2">
-          <i class="fas fa-phone mr-2"></i>
-          <a href="tel:+33148723595">01.48.72.35.95</a>
+          <li class="mb-2">
+            <i class="fas fa-phone mr-2"></i>
+            <a href="tel:+33148723595">01.48.72.35.95</a>
           </li>
-
         </ul>
       </div>
     </div>
   </div>
-</footer> 
+</footer>
